e830a714f497bb44add4e6937a11224d6d74ce7f
ismail-nijazi <ismail.nijazi1@gmail.com>
2023-04-25 19:27:40 +0200

    ...

--- backend/test/integration/test_dao_database_integration.py
+++ backend/test/integration/test_dao_database_integration.py
@@ -9,7 +9,7 @@ from bson.objectid import ObjectId
 @pytest.fixture(scope="module")
 def task():
     dao = DAO(collection_name='task')
-    taskData = {'title': 'hello', 'description': '(add a description here)', 'todos': [], 'startdate': datetime(2022, 7, 2, 11, 24, 36, 467306), 'categories': [], 'video': ObjectId('62c00ed43ef84fb08f8dd096')}
+    taskData = {'title': 'task1', 'description': 'here will be description', 'todos': [], 'startdate': datetime(2023, 6, 2, 11, 24, 36, 1234), 'categories': [], 'video': ObjectId('66b012d43ff84fb0828ad096')}
     task = dao.create(taskData)
     yield task
     dao.delete(task['_id']['$oid'])
@@ -20,23 +20,23 @@ def dao():
     yield dao
 
 def test_case_1(dao, task):
-    data = {'firstName': 'Jane', 'lastName': 'Doe', 'email': 'jane.doe@mail.com', 'tasks': [ObjectId(task['_id']["$oid"])]}
+    data = {'firstName': 'Jane', 'lastName': 'Svenson', 'email': 'test@mail.com', 'tasks': [ObjectId(task['_id']["$oid"])]}
     user = dao.create(data)
     assert user['firstName'] == 'Jane'
     dao.delete(user['_id']['$oid'])
 
 def test_case_2(dao):
     with pytest.raises(Exception):
-        data = {'firstName': 'Jane', 'lastName': 'Doe'}
+        data = {'firstName': 'Jane', 'lastName': 'Svenson'}
         user = dao.create(data)
 
 def test_case_3(dao):
     with pytest.raises(Exception):
-        data = {'firstName': 'Jane', 'lastName': 'Doe', 'email': {'hello': 'world'}}
+        data = {'firstName': 'Jane', 'lastName': 'Svenson', 'email': {'hello': 'world'}}
         user = dao.create(data)
 
 def test_case_4(dao, task):
     with pytest.raises(Exception):
-        data = {'firstName': 'Jane', 'lastName': 'Doe', 'email': 'jane.doe@mail.com', 'tasks': [ObjectId(task['_id']["$oid"])]}
+        data = {'firstName': 'Jane', 'lastName': 'Svenson', 'email': 'test@mail.com', 'tasks': [ObjectId(task['_id']["$oid"])]}
         user = dao.create(data)
         dao.delete(user['_id']['$oid'])
